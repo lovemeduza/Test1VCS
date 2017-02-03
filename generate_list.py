a49327b4598e40d52f8bc7825563f427c07df3b0
Kanpaphat <llovemeduza@gmail.com>
2017-02-03 02:22:05 +0000

    Commit = First version

---
+++ generate_list.py
@@ -0,0 +1,16 @@
+import random
+def generate_list():
+    alist = [x for x in range(random.randint(10,-10)]
+    return alist
+    
+    """
+    print a generatelist
+    """
+    def printIt()
+        print( generate_list() )
+    def main():
+        printIT()
+    
+    if __name__== '__main__':
+        print( "Test printIT():" )
+        main()
